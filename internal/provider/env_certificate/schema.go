c11c9fa2501fd0f2b25517ca716eb1e90e975083
Ignacio Anaya <ignacio.anaya89@gmail.com>
2026-05-04 12:03:17 -0300

    fix: add RequiresReplace plan modifier to certificate env_name
    Changing env_name is an identity change that should force resource
    recreation instead of a silent in-place update.

--- internal/provider/env_certificate/schema.go
+++ internal/provider/env_certificate/schema.go
@@ -7,6 +7,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
@@ -20,6 +22,9 @@ func (r *CertificateResource) Schema(ctx context.Context, req resource.SchemaReq
 				MarkdownDescription: "The name of the environment.",
 				Validators: []validator.String{
 					stringvalidator.LengthAtLeast(1),
+				},
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
 				},
 			},
 			"pem": schema.StringAttribute{
