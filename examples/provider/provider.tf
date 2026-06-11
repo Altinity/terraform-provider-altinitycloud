a05f3459ab9a94cdb0d002b95afa156c7c69ef34
Ignacio Anaya <ignacio.anaya89@gmail.com>
2026-06-11 20:36:38 -0300

    release: v0.7.0

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -3,7 +3,7 @@ terraform {
     altinitycloud = {
       source = "altinity/altinitycloud"
       # https://github.com/altinity/terraform-provider-altinitycloud/blob/master/CHANGELOG.md
-      version = "0.6.1"
+      version = "0.7.0"
     }
   }
 }
